17c7464a1316e443d2c8119da33549c9949ee442
Pedro Gritter <pedro12345g@gmail.com>
2019-08-30 22:04:50 +0100

    Basic React Create App structure

---
+++ drexciya-react-redux/src/Drexciya.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Drexciya.css'
+
+class Drexciya extends Component {
+    render(){
+        return <h1>Welcome to the Bubble Metropolis</h1>
+    }    
+}
+
+export default Drexciya;
