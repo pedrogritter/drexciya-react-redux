7fc043ae0617e0c119ba4f5454140e357b8e7beb
Pedro Gritter <pedro12345g@gmail.com>
2019-09-02 00:01:22 +0100

    Minor changes to loading

--- drexciya-react-redux/src/Drexciya.js
+++ drexciya-react-redux/src/Drexciya.js
@@ -32,26 +32,31 @@ class Drexciya extends Component {
     }  
 
     render() {
-        // Starts with all available items & filters given search term
-        console.log(this.state.users)
-        console.log(this.state.albums)
-        const filteredAlbums = this.state.albums.filter(albums =>{
-            return albums.name.toLowerCase().includes(this.state.searchField.toLowerCase());
-        })
+            // Starts with all available items & filters given search term
+            console.log(this.state.users)
+            console.log(this.state.albums)
+            const filteredAlbums = this.state.albums.filter(albums =>{
+                return albums.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+            })
 
-        return ( 
-            <div className="tc br4 code fl w-100">
-                <h3 className="f1 dark-blue">DREXCIYA RESEARCH LAB</h3>
-                <p className="f3 dark-blue">Slaves thrown overboard give birth underwater to the greatest warriors ever known. The Drexciyans!!</p>
-                <UserCardList users = {this.state.users} />
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList albums={filteredAlbums} />
-                <br></br>
-                <a className="code white" href="https://www.factmag.com/2010/09/09/the-essential-drexciya/"> <h3>The genius of Drexciya in 10 essential releases - by FACTmag</h3></a>
-                <a className="code white" href="https://daily.redbullmusicacademy.com/2017/06/drexciya-infinite-journey-to-inner-space"> <h3>In a sense, to be a Drexciya fan was to build the mythos by yourself. - Kodwo Eshun</h3></a>
-            
-            </div>
-        );
+            if (this.state.users.length === 0) {
+                return <h1>Loading</h1>
+            }
+            else{
+            return ( 
+                <div className="tc br4 code fl w-100">
+                    <h3 className="f1 dark-blue">DREXCIYA RESEARCH LAB</h3>
+                    <p className="f3 dark-blue">Slaves thrown overboard give birth underwater to the greatest warriors ever known. The Drexciyans!!</p>
+                    <UserCardList users = {this.state.users} />
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <CardList albums={filteredAlbums} />
+                    <br></br>
+                    <a className="code white" href="https://www.factmag.com/2010/09/09/the-essential-drexciya/"> <h3>The genius of Drexciya in 10 essential releases - by FACTmag</h3></a>
+                    <a className="code white" href="https://daily.redbullmusicacademy.com/2017/06/drexciya-infinite-journey-to-inner-space"> <h3>In a sense, to be a Drexciya fan was to build the mythos by yourself. - Kodwo Eshun</h3></a>
+                
+                </div>
+            );
+        }
     }    
 }
 
